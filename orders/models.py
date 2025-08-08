691ea277a132e9b1174d6539459115baae3b7cbe
alok-72 <83765662+alok-72@users.noreply.github.com>
2025-08-08 15:35:18 +0530

    16481

--- orders/models.py
+++ orders/models.py
@@ -22,3 +22,29 @@ class OrderItem(models.Model):
     def __str__(self):
         return f"{self.qnty} x {self.item_name}"
 
+
+class UserQueryset(models.QuerySet):
+    def active(self):
+        return self.filter(is_active=True)
+
+class UserManager(models.Manager):
+    def get_queryset(self):
+        return UserQueryset(self.model, using=self._db)
+
+    def active(self):
+        return self.get_queryset().active()
+
+
+
+class user(models.Model):
+    username = models.CharField(max_length=1004, unique=True)
+    email = models.EmailField(unique=True)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+
+    objects = UserManager
+
+    def __str__(self):
+        return self.username
+
