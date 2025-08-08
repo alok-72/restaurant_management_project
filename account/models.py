b3b808a010b007a46f0d0af55b955a926629ee1a
alok-72 <83765662+alok-72@users.noreply.github.com>
2025-08-08 15:16:07 +0530

    16479

--- account/models.py
+++ account/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    total_stock = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+
+    def is_available(self, quantity):
+        if quantity <= 0:
+            return False
+        return self.total_stock >= quantity
